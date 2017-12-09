3b29f4af2d65bb1bdf2f20b92615c0ad9b115f3e
AmanDeep98 <aman2speak@gmail.com>
2017-12-09 20:47:09 +0530

    Buttons and message box added.

--- CrickFev/scripts/cric.py
+++ CrickFev/scripts/cric.py
@@ -1,15 +1,23 @@
 from tkinter import *
 from levent import *
+from lscore import *
 import tkinter.messagebox
 
-def leftclick(self,event):
-	tkinter.messagebox.showinfo('Live Score', tableData[self])
+def leftclick(ch):
+	
+	tkinter.messagebox.showinfo('Live Score',score[ch])
+   
 
 n=len(tableHeads)
+m=len(tableData)
 matches={}
+score={}
 for i in range(n):
 	matches[i]=str(tableHeads[i].h2.text)
-	#print(tableData[i])
+
+for i in range(m):
+    score[i]=tableData[i].section.text	
+
 
 
 class Buttons:
@@ -18,15 +26,22 @@ class Buttons:
 		getLiveEvents()
 		frame=Frame(master)
 		frame.pack()
+		ch=IntVar()
 		#label=Label(frame,text="Live Events",fg="red",sticky=E)
 		#label.grid()
 		for i in range(n):
-			button=Button(frame,text=matches[i],fg="blue")
-			button.bind("<Button-1>", lambda :leftclick(i))
+			button=Button(frame,text=matches[i],fg="blue", )			
 			button.grid(columnspan=5,sticky=W)
-		
+			getScore(i)
 
+		label=Label(frame,text="Enter the event number",fg="red").grid()
+
+		E1=Entry(frame,bd=5,width=3,textvariable=ch)
+		E1.grid()
+		enter=Button(frame,text="Enter",fg="green",bg="violet",command= lambda: leftclick(i))
+		enter.grid()	
 		
+        
         
 
              
